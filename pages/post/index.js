88014e40da02c80f593513aa69ccbfbba6dc6d1f
Pennguyen <ngocandev@gmail.com>
2022-07-26 23:44:46 +0700

    #test api

--- pages/post/index.js
+++ pages/post/index.js
@@ -2,9 +2,11 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router'
 import { useTranslation, Trans } from 'next-i18next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import dbConnect from '../../utils/dbConnect'
+import ModelSubsidize from '../../models/Subsidize';
 
-const HomepagePost = () => {
+const HomepagePost = ({posts}) => {
 
   const router = useRouter()
   const { t } = useTranslation('common')
@@ -27,14 +29,22 @@ const HomepagePost = () => {
         <h3 style={{ minHeight: 70 }}>Index Post</h3>
 
         <div>{data && JSON.stringify(data)}</div>
+        <h3 style={{ minHeight: 70 }}>List Post</h3>
+
+        <div>{posts && JSON.stringify(posts)}</div>
     </main>
   )
 }
+export async function getStaticProps({ locale }){
+  await dbConnect();
+  const data = await ModelSubsidize.find().lean();
 
-export const getStaticProps = async ({ locale }) => ({
-  props: {
-    ...await serverSideTranslations(locale, ['common']),
-  },
-})
+  return {
+    props: {
+      posts: JSON.parse(JSON.stringify(data)),
+      ...await serverSideTranslations(locale, ['common']),
+    },
+  }
+}
 
 export default HomepagePost
