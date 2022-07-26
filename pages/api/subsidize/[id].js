7d56246a7c1ec2bb8958b33064ebdea7d26ae7d1
Pennguyen <ngocandev@gmail.com>
2022-07-26 22:28:50 +0700

    #add api detail

--- pages/api/subsidize/[id].js
+++ pages/api/subsidize/[id].js
@@ -1,30 +1,12 @@
-import { withApiAuthRequired } from '@auth0/nextjs-auth0';
-import * as SubsidizeBranch from '@logics/Subsidize';
+import * as SubsidizeBranch from '../../../logics/Subsidize';
 
-export default withApiAuthRequired(async (req, res) => {
+export default async function subsidizeDetail(req, res){
 
   if (req.method === 'GET') {
-    const branch = await SubsidizeBranch.getDetailById(req.query.id);
+    const branch = await SubsidizeBranch.findById(req.query.id);
 
     return res.status(200).json(branch);
   }
-  /**
-   * Update item by id
-   */
-  if (req.method === 'PUT') {
-    const updateData = req.body;
-
-    // update status
-    if (req.query.updateStatus) {
-      const result = await SubsidizeBranch.updateStatusById(req.query.id, updateData.status);
-
-      return res.status(200).json(result);
-    }
-
-    const result = await SubsidizeBranch.updateById(req.query.id, updateData);
-
-    return res.status(200).json(result);
-  }
   // Lỗi không xác định
   return res.status(404).json({ success: false });
-});
+};
