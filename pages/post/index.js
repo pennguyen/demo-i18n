ba469ee1ba71116536a405593442047de6998fbd
Pennguyen <ngocandev@gmail.com>
2022-07-26 23:28:39 +0700

    #get list posts

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,5 +1,6 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import { useRouter } from 'next/router'
-
 import { useTranslation, Trans } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
@@ -7,11 +8,25 @@ const HomepagePost = () => {
 
   const router = useRouter()
   const { t } = useTranslation('common')
+  const [data, setDetail] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get('/api/subsidize', {
+        params: {},
+      });
+      setDetail(res.data);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <main>
         
         <h3 style={{ minHeight: 70 }}>Index Post</h3>
+
+        <div>{data && JSON.stringify(data)}</div>
     </main>
   )
 }
