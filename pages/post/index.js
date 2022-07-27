79b41e6be6d0b00efa4d552de4c95022e471765d
Pennguyen <ngocandev@gmail.com>
2022-07-27 10:47:17 +0700

    #update 7

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,18 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import useSWR from 'swr'
-import axios from 'axios';
-import { useRouter } from 'next/router'
-import { useTranslation, Trans } from 'next-i18next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { baseUrl } from '../../utils/dbConnect';
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
-const HomepagePost = ({listPost}) => {
+export default function Index() {
   const { data, error } = useSWR('/api/people', fetcher)
-
-
-  const router = useRouter()
-  const { t } = useTranslation('common')
 
   
   if (error) return <div>Failed to load</div>
@@ -27,18 +18,3 @@ const HomepagePost = ({listPost}) => {
     </main>
   )
 }
-
-
-export async function getStaticProps({ locale }){
-  // const res = await fetch(`${baseUrl}api/subsidize`)
-  // const posts = await res.json()
-
-  return {
-    props: {
-      // listPost: posts,
-      ...await serverSideTranslations(locale, ['common']),
-    },
-  }
-}
-
-export default HomepagePost
